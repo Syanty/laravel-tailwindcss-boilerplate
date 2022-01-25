1fdac506e56d4d59e3372d8c0c8b6410e5d21ecc
santosh_tharu <santosh.tharu@volgai.com>
2022-01-25 22:04:20 +0545

    tailwindcss and laravel jetstream/livewire boilerplate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,25 +10,22 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/tailwind.output.css') }}" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-    <script src="{{asset('js/init-alpine.js')}}" defer></script>
+    <script src="{{ asset('js/init-alpine.js') }}" defer></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
-    <script src="{{asset('js/charts-lines.js')}}" defer></script>
-    <script src="{{asset('js/charts-pie.js')}}" defer></script>
-    <script src="{{asset('js/charts-bars.js')}}" defer></script>
+    <script src="{{ asset('js/charts-lines.js') }}" defer></script>
+    <script src="{{ asset('js/charts-pie.js') }}" defer></script>
+    <script src="{{ asset('js/charts-bars.js') }}" defer></script>
 
     @livewireStyles
-    <script>
-        import Turbolinks from 'turbolinks';
-        Turbolinks.start()
-    </script>
 
     <!-- Scripts -->
     {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script> --}}
